6672704978bdeb63809e0f922f40f06fa3e1ac29
Sarah Johnson <sarahviamemphis@gmail.com>
2022-10-29 00:09:04 -0500

    updates to acs variables. created boundaries page

--- census/data/scratch.R
+++ census/data/scratch.R
@@ -1,10 +1,35 @@
 library(tidyverse)
+library(readxl)
 
 v_acs1 <- read_csv("census/data/v_acs1.csv")
 v_acs10 <- read_csv("census/data/v_acs1_10.csv")
+v_acs11 <- read_csv("census/data/v_acs1_11.csv")
+v_acs12 <- read_csv("census/data/v_acs1_12.csv")
 
-hsg <- v_acs1 %>% 
-  filter(str_detect(name, "^B25"))
+v_list <- read_excel("census/data/2021_DataProductList.xlsx") %>% 
+  rename(name = 1,
+         concept = 2,
+         universe = 3,
+         geo1 = 5,
+         geo5 = 6) %>% 
+  mutate(concept = str_to_title(concept),
+         concept = str_replace_all(concept, "\\b20\\d{2}\\b", "ACSYEAR")) %>% 
+  filter(str_detect(name, "^[BC]25"),
+         !str_detect(name, "\\d[:alpha:]"))
+
+# Get the top level variables
+hsg_top <- v_acs1 %>% 
+  filter(str_detect(name, "^[BC]25"),
+         str_detect(name, "_001$"),           #' only top-level variables
+         !str_detect(name, "[:alpha:]_"),     #' omit race variables
+         !str_detect(label, "Estimate!!Aggregate") #' omit aggregates, medians, etc.
+  ) %>% 
+  # Standardize inflation variables across years
+  mutate(name = str_remove_all(name, "_001"),
+         concept = str_to_title(concept),
+    across(c(label, concept), ~ str_replace_all(.x, "\\b20\\d{2}\\b", "ACSYEAR"))) %>% 
+  select(-label) %>%  
+  left_join(v_list)
 
 # Find variables that have changed over the years
 chg <- hsg_top %>% 
@@ -21,13 +46,10 @@ which_hmm <- hsg_top %>%
   filter(!is.na(concept)) %>% 
   filter(str_detect(name, hmm))
 
-hsg2 <- hsg %>% 
-  mutate(across(c(label, concept), ~ str_replace_all(.x, "\\b20\\d{2}\\b", "ACSYEAR")))
+# Clean up
 
-vyr <- hsg2 %>% 
-  count(yr)
-
-hsg3 <- hsg2 %>% 
+hsg3 <- hsg %>% 
+  mutate(across(c(label, concept), ~ str_replace_all(.x, "\\b20\\d{2}\\b", "ACSYEAR"))) %>% 
   filter(str_detect(name, "_001$"),           #' only top-level variables
          !str_detect(name, "[:alpha:]_"),     #' omit race variables
          str_detect(label, "Estimate!!Total") #' omit aggregates, medians, etc.
@@ -78,57 +100,14 @@ wh_yr <- rbind(min_yr, max_yr) %>%
 hsg5 <- hsg3 %>% 
   count(name)
 
-hsg6 <- hsg3 %>% 
-  select(-concept) %>% 
-  
 
-v05 <- hsg3 %>% 
-  filter(yr == 2005) %>% 
-  select(-yr)
-v06 <- hsg3 %>% 
-  filter(yr == 2006) %>% 
-  select(-yr)
-v07 <- hsg3 %>% 
-  filter(yr == 2007) %>% 
-  select(-yr)
-v08 <- hsg3 %>% 
-  filter(yr == 2008) %>% 
-  select(-yr)
-v09 <- hsg3 %>% 
-  filter(yr == 2009) %>% 
-  select(-yr)
-v10 <- hsg3 %>% 
-  filter(yr == 2010) %>% 
-  select(-yr)
-v11 <- hsg3 %>% 
-  filter(yr == 2011) %>% 
-  select(-yr)
-v12 <- hsg3 %>% 
-  filter(yr == 2012) %>% 
-  select(-yr)
-v13 <- hsg3 %>% 
-  filter(yr == 2013) %>% 
-  select(-yr)
-v14 <- hsg3 %>% 
-  filter(yr == 2014) %>% 
-  select(-yr)
-v15 <- hsg3 %>% 
-  filter(yr == 2015) %>% 
-  select(-yr)
-v16 <- hsg3 %>% 
-  filter(yr == 2016) %>% 
-  select(-yr)
-v17 <- hsg3 %>% 
-  filter(yr == 2017) %>% 
-  select(-yr)
-v18 <- hsg3 %>% 
-  filter(yr == 2018) %>% 
-  select(-yr)
-v19 <- hsg3 %>% 
-  filter(yr == 2019) %>% 
-  select(-yr)
-v21 <- hsg3 %>% 
-  filter(yr == 2021) %>% 
-  select(-yr)
+v_acs_changes <- read_csv("census/data/2021-1yr-api-changes.csv") %>% 
+  rename(tblID = 2)
 
+v_hsg_chg <- v_acs_changes %>% 
+  filter(str_starts(tblID, "B25"))
 
+v_hsg_chg %>% count(Change)
+
+updated21 <- v_hsg_chg %>% filter(Change == "Variable Updated")
+no_match_21 <- v_hsg_chg %>% filter(Change == "No Match")
